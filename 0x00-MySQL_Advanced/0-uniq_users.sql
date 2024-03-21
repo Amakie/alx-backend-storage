4c3a9504f8b090540acfadb7cce8c2f596e34df0
Constance Amatey <consamate57@gmail.com>
2024-03-21 18:37:56 +0000

    task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,6 @@
 --SQL script that creates a table users following these requirements
---attributes are id, email, name
 CREATE TABLE IF NOT EXITS users (
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email varchar(255) NOT NULL UNIQUE,
 	name varchar(255)
-)
+);
